04e342201aad370aa000a09726f4ca9d401af30c
James Hazelwood <hazelwoo@unix5.csc.calpoly.edu>
2024-12-07 03:17:08 -0800

    close

--- Makefile
+++ Makefile
@@ -3,7 +3,10 @@ CFLAGS = -Wall -std=c99 -pedantic
 
 
 all : 
-	$(CC) $(CFLAGS) main.c -o main.out
+	$(CC) $(CFLAGS) main.c -o main.out -g
 
 test : 
-	./main.out small.csv ops.text
+	./main.out small.csv ops.txt
+
+valgrind :
+	valgrind --leak-check=yes ./main.out small.csv ops.txt
--- main.c
+++ main.c
@@ -16,7 +16,37 @@ struct Row {
     char *county;
     char *state;
     float *nums;
+    int valid;
 };
+
+void freeArray(struct Array *arr) {
+
+    for (int i = 0; i < arr->items; i++) {
+        free(arr->strings[i]);
+    }
+    
+    free(arr->strings);
+    
+    arr->strings = NULL;
+    arr->capacity = 0;
+    arr->items = 0;
+    
+    free(arr);
+}
+
+void freeRow(struct Row *row) {
+
+    free(row->county);
+    free(row->state);
+    free(row->nums);
+    
+    row->county = NULL;
+    row->state = NULL;
+    row->nums = NULL;
+    row->valid = 0;
+
+    free(row);
+}
 
 int main(int argc, char *argv[]){
 
@@ -27,27 +57,34 @@ int main(int argc, char *argv[]){
     FILE *csv_file = fopen(argv[1], "r");
     if (csv_file == NULL) {
         perror("Error opening CSV file");
+        return 1;
     }
 
     FILE *ops_file = fopen(argv[2], "r");
     if (ops_file == NULL) {
         perror("Error opening OPS file");
+        return 1;
     }
 
-    char *line = NULL;
-    unsigned int len;
-
-    if (getline(&line, (size_t *)&len, csv_file) == -1) {
-        perror("Error getting line");
-    }
 
     struct Array *header = malloc(sizeof(struct Array));
     header->strings = malloc(sizeof(char *) * 10);
     header->items = 0;
     header-> capacity = 10;
 
+    char *line = NULL;
+    size_t len = 0;
+    if (getline(&line, (size_t *)&len, csv_file) == -1) {
+        perror("Error getting line");
+        free(line);
+        return 1;
+    }
+
     char *args;
     while((args = strsep(&line, ",")) != NULL){
+
+        args[strlen(args) - 1] = '\0';
+        args++;
 
         if(header->items >= header->capacity){
             header->capacity += 10;
@@ -58,18 +95,24 @@ int main(int argc, char *argv[]){
         header->items += 1;
     }
 
+    free(line);
+
     struct Row *counties[5000];
     int i = 0;
+    int j = 0;
+    int rows = 0;
 
     while (getline(&line, (size_t *)&len, csv_file) != -1) {
+        rows++;
         counties[i] = malloc(sizeof(struct Row));
+        counties[i]->valid = 1;
         args = strsep(&line, ",");
         counties[i]->county = strdup(args);
         args = strsep(&line, ",");
         counties[i]->state = strdup(args);
         float *nums_array = malloc(sizeof(float) * header->items);
-        int j = 0;
 
+        j = 0;
         while((args = strsep(&line, ",")) != NULL){
             args[strlen(args) - 1] = '\0';
             args++;
@@ -81,5 +124,164 @@ int main(int argc, char *argv[]){
         i++;
     }
 
-    printf("%s   %f\n", counties[4]->county, counties[4]->nums[4]);
+    // get population index (cause we use that alot)
+    int pop_index = -1;
+    for(i = 0; i < header->items; i++){
+        if (strcmp(header->strings[i], "Population.2014 Population") == 0){
+            pop_index = i - 2;
+            break;
+        }
+    }
+
+    int sum;
+    int sum2;
+    int temp;
+    int temp2;
+    float percent;
+    int index; 
+
+    while (getline(&line, (size_t *)&len, ops_file) != -1) {
+
+        if (line[strlen(line) - 1] == '\n') {
+            line[strlen(line) - 1] = '\0';
+        }
+
+        // display
+        if (strcmp(line, "display") == 0) {
+            for(i = 0; i < rows; i++) {
+                if(counties[i]->valid) {
+                    printf("%s %s: ", counties[i]->county, counties[i]->state);
+                    for (j = 0; j < header->items - 2; j++) {
+                        printf("%s: %.1f ", header->strings[j + 2], counties[i]->nums[j]);
+                    }
+                    printf("\n");
+                }
+            }
+            continue;
+
+        // population-total
+        } else if (strcmp(line, "population-total") == 0) {
+            
+            if (pop_index == -1) {
+                printf("Cant find population total");
+            } else {
+                sum = 0;
+                for(i = 0; i < rows; i++) {
+                    if(counties[i]->valid) {
+                        sum += counties[i]->nums[pop_index];
+                    }
+                }
+                printf("2014 Population: %i\n", sum);
+                continue;
+            }
+        }
+
+        args = strsep(&line, ":");
+
+        if (args == NULL) {
+            printf("Error geting Line");
+            continue;
+        }
+
+        // population:<field>
+        // percent:<field>
+        if (strcmp(args, "population") == 0 || strcmp(args, "percent") == 0) {
+            index = -1;
+            for(i = 0; i < header->items; i++){
+                if (strcmp(header->strings[i], line) == 0){
+                    index = i - 2;
+                    break;
+                }   
+            }
+
+            if (index == -1) {
+                printf("Cant find population total");
+                continue;
+            }
+
+            sum = 0;
+            for(i = 0; i < rows; i++) {
+                if(counties[i]->valid) {
+                    sum += counties[i]->nums[index];
+                }
+            }
+
+            if (strcmp(args, "population") == 0) {
+                printf("%s Population: %i\n", line, sum);
+            } else {
+                sum2 = 0;
+                for(i = 0; i < rows; i++) {
+                    if(counties[i]->valid) {
+                        sum2 += counties[i]->nums[pop_index];
+                    }
+                }
+                percent = (float)sum / sum2;
+                printf("%s Population Percentage: %f\n", line, percent);
+            }
+            continue;
+        
+        // filter-state:<state abbreviation>
+        } else if (strcmp(args, "filter-state") == 0) {
+            for(i = 0; i < rows; i++) {
+                if(counties[i]->valid && strcmp(counties[i]->state, line)) {
+                    counties[i]->valid = 0;
+                }
+            }
+            continue;
+
+        // filter:<field>:<ge/le>:<number>
+        } else if (strcmp(args, "filter") == 0) {
+            char *field = strsep(&line, ":");
+            char *gele = strsep(&line, ":");
+            char *number = strsep(&line, ":");
+
+            if (field == NULL || gele == NULL || number == NULL) {
+                printf("Error getting line");
+                continue;
+            }
+
+            temp2 = atoi(number);
+
+            index = -1;
+            for(i = 0; i < header->items; i++){
+                if (strcmp(header->strings[i], field) == 0){
+                    index = i - 2;
+                    break;
+                }   
+            }
+
+            if (index == -1) {
+                printf("Cant find population total");
+                continue;
+            }
+            
+            temp = 0;
+            if (strcmp(gele, "ge") == 0) {
+                temp = -1;
+            } else if (strcmp(gele, "le") == 0) {
+                temp = 1;
+            } else {
+                printf("Couldn't get ge/le");
+                continue;
+            }
+
+            for(i = 0; i < rows; i++) {
+                if(counties[i]->nums[index] * temp >= temp2 * temp) {
+                    counties[i]->valid = 0;
+                }
+            }
+
+            continue;
+        }
+    }
+
+    freeArray(header);
+    
+    for(i = 0; i < rows; i++) {
+        freeRow(counties[i]);
+    }
+
+    fclose(csv_file);
+    fclose(ops_file);
+
 }
